2f5bc9606893fdd8aaa4013cc7e82fa8ad69a41d
Dan <pintea.dan988@gmail.com>
2023-03-26 12:17:33 +0200

    Implemented all TurbineDetailPage.jsx

--- test-components/src/pages/Turbines/TurbineDetailPage/TurbineDetailPage.jsx
+++ test-components/src/pages/Turbines/TurbineDetailPage/TurbineDetailPage.jsx
@@ -2,24 +2,30 @@ import React, {useEffect, useState} from 'react';
 
 import Header from "../../../components/Header/Header.jsx";
 import HeaderTextMK3 from "../../../components/HeaderText/MK3/HeaderTextMK3.jsx";
-import TurbineDemo from "../../../components/TurbineDemo/TurbineDemo.jsx";
 import Map from "../../../components/Maps/Map.jsx";
 import ContentContainer from "../../../components/Containers/ContentContainer/ContentContainer.jsx";
 
 import styles from './TurbineDetailPage.module.scss';
 import installations from '../../../utils/demo-data/installations.json'
+import services from '../../../utils/demo-data/services.json'
+import commissions from '../../../utils/demo-data/commisions.json'
 
 function TurbineDetailPage({projectId, projectName, storedTurbineName}) {
-    const [installationsCount, setInstallationsCount] = useState([]);
+
+    /*--------------------------------------------------------------------------------------------*/
+
+    // Installations data
+    const [installationsID, setInstallationsID] = useState([]);
     const [installationsDate, setInstallationsDate] = useState([]);
     const [installationsNetDuration, setInstallationsNetDuration] = useState([]);
     const [installationsDeviation, setInstallationsDeviation] = useState([]);
+
 
     // Function that filters the installations data from the API based on the turbine ID
     useEffect(() => {
         installations.map((installation) => {
             if (installation.Turbine_ID === storedTurbineName) {
-                setInstallationsCount(installation.Installation_ID)
+                setInstallationsID(installation.Installation_ID)
                 setInstallationsDate(formatDate(installation.Installation_Date))
                 setInstallationsNetDuration(installation.Installation_NET_Duration)
                 setInstallationsDeviation(installation.Installation_Deviations)
@@ -41,6 +47,59 @@ function TurbineDetailPage({projectId, projectName, storedTurbineName}) {
         return `${day} ${monthName} 20${year}`;
     }
 
+    /*--------------------------------------------------------------------------------------------*/
+
+    // Service data
+    const [servicesID, setServicesID] = useState([]);
+    const [servicesDate, setServicesDate] = useState([]);
+    const [servicesNetDuration, setServicesNetDuration] = useState([]);
+    const [servicesDeviation, setServicesDeviation] = useState([]);
+
+    // Function that filters the services data from the API based on the turbine ID
+    useEffect(() => {
+        services.map((service) => {
+            if (service.Turbine_ID === storedTurbineName) {
+                setServicesID(service.Service_ID)
+                setServicesDate(formatDateWithoutAddedYear(service.Service_Date))
+                setServicesNetDuration(service.Service_NET_Duration)
+                setServicesDeviation(service.Service_Deviations)
+            }
+        })
+    }, []);
+
+    // Function that formats the date from the API to a more readable format
+    function formatDateWithoutAddedYear(dateString) {
+        const [month, day, year] = dateString.split('/');
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const monthName = monthNames.map((name, index) => {
+            if (index + 1 === parseInt(month)) {
+                return name;
+            }
+        }).filter(Boolean)[0];
+        return `${day} ${monthName} ${year}`;
+    }
+
+    /*--------------------------------------------------------------------------------------------*/
+
+    // Commissioning data
+    const [commissionID, setCommissionID] = useState([]);
+    const [commissionsDate, setCommissionsDate] = useState([]);
+    const [commissionsNetDuration, setCommissionsNetDuration] = useState([]);
+    const [commissionsDeviation, setCommissionsDeviation] = useState([]);
+
+    // Function that filters the commission data from the API based on the turbine ID
+    useEffect(() => {
+        commissions.map((commission) => {
+            if (commission.Turbine_ID === storedTurbineName) {
+                setCommissionID(commission.Comm_ID)
+                setCommissionsDate(formatDateWithoutAddedYear(commission.Comm_Date))
+                setCommissionsNetDuration(commission.Comm_NET_Duration)
+                setCommissionsDeviation(commission.Comm_Deviations)
+            }
+        })
+    }, []);
 
     return (
         <>
@@ -55,11 +114,12 @@ function TurbineDetailPage({projectId, projectName, storedTurbineName}) {
                         turbineId={`${storedTurbineName}`}
                     />
                 </div>
+
                 <div className={styles.turbines_detail_grid}>
                     <div className={styles.turbines_detail_row}>
                         <ContentContainer containerTitle={"Installations"}
                                           fieldTitleOne={"Installation ID"}
-                                          fieldValueOne={installationsCount}
+                                          fieldValueOne={installationsID}
                                           fieldTitleTwo={"Installation Date"}
                                           fieldValueTwo={installationsDate}
                                           fieldTitleThree={"Installation NET Duration"}
@@ -70,15 +130,32 @@ function TurbineDetailPage({projectId, projectName, storedTurbineName}) {
                     </div>
 
                     <div className={styles.turbines_detail_row}>
-                        <TurbineDemo/>
+                        <ContentContainer containerTitle={"Services"}
+                                          fieldTitleOne={"Service ID"}
+                                          fieldValueOne={servicesID}
+                                          fieldTitleTwo={"Service Date"}
+                                          fieldValueTwo={servicesDate}
+                                          fieldTitleThree={"Service NET Duration"}
+                                          fieldValueThree={servicesNetDuration}
+                                          fieldTitleFour={"Service Deviation"}
+                                          fieldValueFour={(Math.round(servicesDeviation * 100) / 100).toFixed(2)}
+                        />
                     </div>
 
                     <div className={styles.turbines_detail_row}>
-                        <TurbineDemo/>
+                        <ContentContainer containerTitle={"Commissioning"}
+                                          fieldTitleOne={"Commission ID"}
+                                          fieldValueOne={commissionID}
+                                          fieldTitleTwo={"Commission Date"}
+                                          fieldValueTwo={commissionsDate}
+                                          fieldTitleThree={"Commission NET Duration"}
+                                          fieldValueThree={commissionsNetDuration}
+                                          fieldTitleFour={"Commission Deviation"}
+                                          fieldValueFour={(Math.round(commissionsDeviation * 100) / 100).toFixed(2)}
+                        />
                     </div>
-
-
                 </div>
+
                 <div className={styles.turbines_details_maps_container}>
                     <div className={styles.turbines_details_maps_container_inner}>
                         <Map/>
