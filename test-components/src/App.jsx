510417a3cdf5751262e39ccefc5e6c8322bb31f7
Dan <pintea.dan988@gmail.com>
2023-03-27 14:52:19 +0200

    Adding charts.

--- test-components/src/App.jsx
+++ test-components/src/App.jsx
@@ -36,8 +36,6 @@ function App() {
                 flexDirection: "column"
             }}>
                 <PulsatingCircle/>
-                {/*<SelectDemo/>*/}
-                {/*<CalendarDemo/>*/}
                 {/*<ContainerData/>*/}
             </div>}/>
         </Routes>
